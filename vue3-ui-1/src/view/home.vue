0d0ae884d71a07869f8b6ae1f7aa7c3a376377b5
lwwywang <liwenwang4work@163.com>
2024-01-17 15:43:02 +0800

    初始化aside的html

--- vue3-ui-1/src/view/home.vue
+++ vue3-ui-1/src/view/home.vue
@@ -5,8 +5,8 @@
             <h1>Liwen的UI框架</h1>
             <h2>这是一个厉害的UI框架</h2>
             <p class="actions">
-                <a>Github</a>
-                <a>开始</a>
+                <a href="https://github.com">Github</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
@@ -18,4 +18,13 @@ export default {
     components: { topnav }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+}
+</style>
