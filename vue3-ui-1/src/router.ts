b199a3b4c7c9a07c87976e50af6623114b0aabee
lwwywang <liwenwang4work@163.com>
2024-01-17 21:47:45 +0800

    基本完成官网框架

--- vue3-ui-1/src/router.ts
+++ vue3-ui-1/src/router.ts
@@ -2,18 +2,31 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 import home from './view/home.vue'
 import doc from './view/doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
 const history = createWebHashHistory()
 export const router = createRouter({
     history: history,
     routes: [
-        { path: '/', component: home },
-        { path: '/doc', component: doc, children:[
-            {path: 'switch', component: SwitchDemo}
-        ] }
+        { path: "/", component: home },
+        {
+            path: "/doc",
+            component: doc,
+            children: [
+                { path: "", component: DocDemo },
+                { path: "switch", component: SwitchDemo },
+                { path: "button", component: ButtonDemo },
+                { path: "dialog", component: DialogDemo },
+                { path: "tabs", component: TabsDemo },
+            ],
+        },
+
     ]
 })
 
 router.afterEach(() => {
     console.log("路由切换了");
-  });
+});
