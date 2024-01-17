69dee1144aa496229c9e3224771c1c4a54e75b97
lwwywang <liwenwang4work@163.com>
2024-01-17 10:50:56 +0800

    topnav样式更新

---
+++ vue3-ui-1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> hi </div>
+  <Liwen />
+</template>
+
+<script>
+import Liwen from './components/Liwen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Liwen
+  }
+}
+</script>
