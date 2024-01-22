158f3ee17048a3036a18df3fafa07b30e824a54a
lwwywang <liwenwang4work@163.com>
2024-01-22 16:53:29 +0800

    更改css样式

--- vue3-ui-1/src/view/doc.vue
+++ vue3-ui-1/src/view/doc.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="layout">
-        <topnav class="nav"/>
+        <topnav class="nav" />
         <div class="content">
             <aside v-if="asideVisible">
                 <h2>组件列表</h2>
@@ -40,51 +40,61 @@ export default {
 
 <style lang="scss" scoped>
 .layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  > .nav {
-    flex-shrink: 0;
-  }
-  > .content {
-    flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
-    @media (max-width: 500px) {
-      padding-left: 0; 
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+
+    >.nav {
+        flex-shrink: 0;
     }
-  }
+
+    >.content {
+        flex-grow: 1;
+        padding-top: 60px;
+        padding-left: 156px;
+
+        @media (max-width: 500px) {
+            padding-left: 0;
+        }
+    }
 }
+
 .content {
-  display: flex;
-  > aside {
-    flex-shrink: 0;
-  }
-  > main {
-    flex-grow: 1;
-    padding: 16px;
-    background: lightgreen;
-  }
-}
-aside {
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-  > h2 {
-    margin-bottom: 4px;
-  }
-  > ol {
-    > li {
-      padding: 4px 0;
+    display: flex;
+
+    >aside {
+        flex-shrink: 0;
     }
-  }
+
+    >main {
+        flex-grow: 1;
+        padding: 16px;
+        background: white;
+        ;
+    }
 }
+
+aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+    height: 100%;
+
+    >h2 {
+        margin-bottom: 4px;
+    }
+
+    >ol {
+        >li {
+            padding: 4px 0;
+        }
+    }
+}
+
 main {
-  overflow: auto;
-}
-</style>
+    overflow: auto;
+}</style>
