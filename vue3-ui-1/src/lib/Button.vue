a58f72d07ebde35abf6bd8fdaab4ed130bf6438f
lwwywang <liwenwang4work@163.com>
2024-01-24 15:57:56 +0800

    css最小影响原则

--- vue3-ui-1/src/lib/Button.vue
+++ vue3-ui-1/src/lib/Button.vue
@@ -54,5 +54,28 @@ $radius: 4px;
     &::-moz-focus-inner {
         border: 0;
     }
+
+    &.liwen-theme-link {
+        border-color: transparent;
+        box-shadow: none;
+        color: $blue;
+
+        &:hover,
+        &:focus {
+            color: lighten($blue, 10%);
+        }
+    }
+
+    &.liwen-theme-text {
+        border-color: transparent;
+        box-shadow: none;
+        color: inherit;
+
+        &:hover,
+        &:focus {
+            background: darken(white, 5%);
+            ;
+        }
+    }
 }
 </style>
