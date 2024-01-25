799164aeef2cae62831c1ae4672dc4121783e0e6
lwwywang <liwenwang4work@163.com>
2024-01-25 10:33:29 +0800

    支持disabled按钮

--- vue3-ui-1/src/lib/Button.vue
+++ vue3-ui-1/src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="liwen-button" :class="classes">
+    <button class="liwen-button" :class="classes" :disabled="disabled">
         <slot />
     </button>
 </template>
@@ -19,6 +19,10 @@ export default {
         level: {
             type: String,
             default: "normal",
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
         },
     },
     setup(props) {
@@ -42,6 +46,7 @@ $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 $red: red;
+$grey: grey;
 
 .liwen-button {
     box-sizing: border-box;
@@ -142,6 +147,25 @@ $red: red;
             &:focus {
                 color: darken($red, 10%);
             }
+        }
+    }
+
+    &.liwen-theme-button {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+
+            &:hover {
+                border-color: $grey;
+            }
+        }
+    }
+
+    &.liwen-theme-link,
+    &.liwen-theme-text {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
         }
     }
 }</style>
