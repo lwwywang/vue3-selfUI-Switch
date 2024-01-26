bb9651f863a6ff29b43fa5a9af39d61f66fe547e
lwwywang <liwenwang4work@163.com>
2024-01-26 19:57:29 +0800

    dialog点击toggle能触发visible

--- vue3-ui-1/src/lib/Dialog.vue
+++ vue3-ui-1/src/lib/Dialog.vue
@@ -1,23 +1,32 @@
 <template>
-    <div class="liwen-dialog-overlay"></div>
-    <div class="liwen-dialog-wrapper">
-        <div class="liwen-dialog">
-            <header>标题</header>
-            <main>
-                <p>第一行字</p>
-                <p>第二行字</p>
-            </main>
-            <footer>
-                <Button>ok</Button>
-                <Button>cancel</Button>
-            </footer>
+    <template v-if="visible">
+        <div class="liwen-dialog-overlay"></div>
+        <div class="liwen-dialog-wrapper">
+            <div class="liwen-dialog">
+                <header>标题</header>
+                <main>
+                    <p>第一行字</p>
+                    <p>第二行字</p>
+                </main>
+                <footer>
+                    <Button>ok</Button>
+                    <Button>cancel</Button>
+                </footer>
+            </div>
         </div>
-    </div>
+    </template>
 </template>
+    
 
 <script lang="ts">
 import Button from "./Button.vue"
 export default {
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        }
+    },
     components: { Button }
 }
 </script>
