4327753d661c097e245956b1b7eec24dcd2cd6e7
lwwywang <liwenwang4work@163.com>
2024-01-30 05:01:25 +0800

    优化2

--- vue3-ui-1/src/lib/Tabs.vue
+++ vue3-ui-1/src/lib/Tabs.vue
@@ -54,11 +54,6 @@ export default {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
         })
-        const current = computed(() => {
-            return defaults.filter((tag) => {
-                return tag.props.title === props.selected
-            })[0]
-        })
         const titles = defaults.map((tag) => {
             return tag.props.title
         })
@@ -68,7 +63,6 @@ export default {
         return {
             defaults,
             titles,
-            current,
             select,
             selectedItem,
             indicator,
