721b69cb338ca0a1c7a5c6620aff76e724044968
lwwywang <liwenwang4work@163.com>
2024-01-30 17:27:55 +0800

    给文字和链接添加样式

--- vue3-ui-1/src/components/topnav.vue
+++ vue3-ui-1/src/components/topnav.vue
@@ -24,7 +24,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
+
 .topnav {
+    color: $color;
     display: flex;
     padding: 16px;
     position: fixed;
--- vue3-ui-1/src/view/home.vue
+++ vue3-ui-1/src/view/home.vue
@@ -19,15 +19,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
 .topnavAndBanner {
     background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
 }
 
 .banner {
+    color: $color;
     padding: 100px 0;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    >.actions {
+        padding: 8px 0;
+
+        a {
+            margin: 0 8px;
+            background: $green;
+            color: white;
+            display: inline-block;
+            padding: 8px 24px;
+            border-radius: $border-radius;
+
+            &:hover {
+                text-decoration: none;
+            }
+        }
+    }
 }
 </style>
