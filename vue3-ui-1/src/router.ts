634ecfac81df40cc4ad9f68feee89208a665315e
lwwywang <liwenwang4work@163.com>
2024-01-30 19:55:23 +0800

    创建路由

--- vue3-ui-1/src/router.ts
+++ vue3-ui-1/src/router.ts
@@ -6,6 +6,9 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
+import Intro from './view/Intro.vue';
+import GetStarted from './view/GetStarted.vue'
+import Install from './view/Install.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -17,6 +20,9 @@ export const router = createRouter({
             component: doc,
             children: [
                 { path: "", component: DocDemo },
+                { path: "intro", component: Intro },
+                { path: "get-started", component: GetStarted },
+                { path: "install", component: Install },
                 { path: "switch", component: SwitchDemo },
                 { path: "button", component: ButtonDemo },
                 { path: "dialog", component: DialogDemo },
