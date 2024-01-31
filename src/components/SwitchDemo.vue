094fcd6a7b409844789c6a316308e66564919677
lwwywang <liwenwang4work@163.com>
2024-01-31 21:36:28 +0800

    展示多种颜色的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,8 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch1Demo.__sourceCode }}</pre>
+                <pre class="language-html"
+                    v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
             </div>
         </div>
         <div class="demo">
@@ -22,7 +23,8 @@
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch2Demo.__sourceCode }}</pre>
+                <pre class="language-html"
+                    v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
             </div>
         </div>
     </div>
@@ -36,7 +38,9 @@ import {
 } from 'vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-console.log(Switch1Demo.__sourceCode)
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism
 
 export default {
     components: {
@@ -47,7 +51,8 @@ export default {
         return {
             bool,
             Switch1Demo,
-            Switch2Demo
+            Switch2Demo,
+            Prism
         }
     }
 }
