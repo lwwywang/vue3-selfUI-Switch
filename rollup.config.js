7d6f399aa3090751f3d424fd49596892f6c0d3bd
lwwywang <liwenwang4work@163.com>
2024-04-02 23:59:01 +0800

    更新package.json

--- rollup.config.js
+++ rollup.config.js
@@ -10,8 +10,8 @@ export default {
         globals: {
             vue: 'Vue'
         },
-        name: 'Gulu',
-        file: 'dist/lib/gulu.js',
+        name: 'SX',
+        file: 'dist/lib/SX.js',
         format: 'umd',
         plugins: [terser()]
     },
