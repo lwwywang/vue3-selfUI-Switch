2a5351a81b1fb9baf0d49318d943a2d1a94aa42e
lwwywang <liwenwang4work@163.com>
2024-04-04 16:10:55 +0800

    更新rollup上传npm

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,25 @@
 import esbuild from 'rollup-plugin-esbuild'
-import vue from 'rollup-plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import scss from 'rollup-plugin-scss'
 import dartSass from 'sass';
 import { terser } from "rollup-plugin-terser"
 
 export default {
     input: 'src/lib/index.ts',
-    output: {
+    output: [{
         globals: {
             vue: 'Vue'
         },
-        name: 'SX',
-        file: 'dist/lib/SX.js',
+        name: 'shanxue',
+        file: 'dist/lib/shanxue.js',
         format: 'umd',
         plugins: [terser()]
-    },
+    }, {
+        name: 'shanxue',
+        file: 'dist/lib/shanxue.esm.js',
+        format: 'es',
+        plugins: [terser()]
+    }],
     plugins: [
         scss({ include: /\.scss$/, sass: dartSass }),
         esbuild({
